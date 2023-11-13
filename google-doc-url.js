d3a8cbb7f3b4468005ec98d253cba9badac09741
KacperZGolebia <150247950+KacperZGolebia@users.noreply.github.com>
2023-11-13 23:21:54 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1OoFCwWSUcJvmGD4jmUu43JL4hWcWoWxBCxykvipgpwg/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR6vPMI3SfXOyE6xbBJyi3LDVLmm4WagCdDcCCky7iChKScgWxTZroyAVGFX35tp1WKmDJ9clkXOVJU/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
